b3cd74eff569ac73f6777f376910bff5652abee5
Влада <gibinav@mail.ru>
2022-10-16 10:20:31 +0300

    g

--- project/views/auth/user.py
+++ project/views/auth/user.py
@@ -32,24 +32,6 @@ class LoginView(Resource):
     def put(self):
         data = request.json
         header = request.headers.environ.get('HTTP_AUTHORIZATION').replace('Bearer ', '')
-        return user_service.update(data=data, refresh_token=header)
+        return user_service.update_password(data=data, refresh_token=header)
 
 
-@api.route('/<int:bid>')
-class UsersView(Resource):
-    def get(self, bid):
-        b = user_service.get_one(bid)
-        sm_d = UserSchema().dump(b)
-        return sm_d, 200
-
-    def put(self, bid):
-        req_json = request.json
-        if "id" not in req_json:
-            req_json["id"] = bid
-        user_service.update(req_json)
-        return "", 204
-
-
-    def delete(self, bid):
-        user_service.delete(bid)
-        return "", 204
